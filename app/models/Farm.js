9a368c99753d5d199008b241e968070027377e55
KOSASIH <kosasihg88@gmail.com>
2024-07-07 16:30:29 +0700

    Create Farm.js

---
+++ app/models/Farm.js
@@ -0,0 +1,11 @@
+class Farm {
+  constructor(id, name, description, image, location) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.image = image;
+    this.location = location;
+  }
+}
+
+export default Farm;
