f1c822e32c8eb6889191932508b689c23315c6e1
KOSASIH <kosasihg88@gmail.com>
2024-07-07 16:26:51 +0700

    Create environment.js

---
+++ config/environment.js
@@ -0,0 +1,5 @@
+module.exports = {
+  API_URL: 'https://api.global-food-and-agricultural.com',
+  MAPBOX_TOKEN: 'YOUR_MAPBOX_TOKEN',
+  DATABASE_URL: 'mongodb://localhost:27017/global-food-and-agricultural',
+};
