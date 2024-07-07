90cf432e396ea62ed57cf2ea13c41abfbad0c1de
KOSASIH <kosasihg88@gmail.com>
2024-07-07 16:31:19 +0700

    Create constants.js

---
+++ app/utils/constants.js
@@ -0,0 +1,10 @@
+export const FARM_TYPES = {
+  ORGANIC: 'organic',
+  CONVENTIONAL: 'conventional',
+};
+
+export const FARM_CATEGORIES = {
+  FRUITS: 'fruits',
+  VEGETABLES: 'vegetables',
+  GRAINS: 'grains',
+};
